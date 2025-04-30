024ea376b535c10123939c8a8c3f236ab0bfc4b6
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2025-04-30 14:22:53 -0400

    1

--- Final.py
+++ Final.py
@@ -3,7 +3,7 @@ class book():
         self.name = "Tjay"
         self.title = "T"
         self.price = 0
-        self.Book = {}
+        self.book = {}
 
 
     def Add(self):
@@ -11,6 +11,23 @@ class book():
         self.author = input("enter the author of the book you add: \n").upper
         self.price = float(input("Enter the listing price the book: \n"))
 
+        print("you have added", self.title)
+        print(self.book)
 
-        self.Book[self.title] = {'Auther':self.author, 'price':self.price}
-        print(self.Book)
+def Muins(self):
+    self.title = input("enter the name of the bookyou remove: \n")
+    try:
+        str(self.title)
+        if self.title in self.book:
+            del self.book[self.title]
+            print(self.book)
+
+    except:
+        print("Please enter a book title")
+
+
+
+def showbook(self):
+    for x in self.book:
+        print(x)
+        print(self.book[x]["aother"])
