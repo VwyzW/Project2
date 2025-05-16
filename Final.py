29d9be4b3037da0cd8365dc58de246a1e27e96be
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2025-05-16 14:27:46 -0400

    1

--- Final.py
+++ Final.py
@@ -1,7 +1,7 @@
 class book():
     def __init__(self):
         self.index_file = 'index.txt'
-        
+
         try:
             with open(self.index_file, 'r', encoding='utf-8') as f:
                 self.title = [line.strip().upper() for line in f if line.strip()]
@@ -17,7 +17,7 @@ class book():
 
     def Add(self):
         title = input("Enter the name of the book you add: \n").strip().upper()
-        if title in self.title :
+        if title in self.titles:
             print(f"Book '{title}' already exists.\n")
             return
         self.author = input("Enter the author of the book you add: \n").strip()
@@ -27,27 +27,28 @@ class book():
             print("The price format is incorrect, failed to add。")
             return
         fname = f"{title}.txt"
-        with open(fname, 'w' , encoding='uutf-8') as f:
+        with open(fname ,'w' ,encoding='utf-8') as f:
             f.write(f"author: {self.author}\n")
             f.write(f"price: {self.price}\n")
         
         self.titles.append(title)
         self.save
-        print(f"you have added {title}.\n")
+        
+        print(f"You have added '{title}'.\n")
         
     def Minus(self):
-        title = input("enter the name of the bookyou remove: \n").strip().upper()
-        if title not in self.Books:
-            print(f"Book not found: {title}\n")
+        title = input("Enter the name of the bookyou remove: \n").strip().upper()
+        if title not in self.titles:
+            print(f"Book not found: {title}\n.")
             return
-        self.titles.append(title)
-        self.save()
+        self.titles.remove(title)
+        self.save
 
     def showbook(self):
         if not self.titles:
             print("No books in library.\n")
             return
-        for title in self.titles():
+        for title in self.titles:
             fname = f"{title}.txt"
             print(f"TITLE : {title}")
             try:
--- VW.py
+++ VW.py
@@ -4,7 +4,7 @@ def main():
     b = book()
 
     while True:
-        v = input("Would you like to add a book?'yes or no (show or delete)'").strip().lower()
+        v = input("Would you like to add a book?'yes or no (show or delete(remove))'").strip().lower()
         if v == 'yes':
             b.Add() 
         
@@ -15,11 +15,11 @@ def main():
         elif v == 'show':
             b.showbook()
 
-        elif v == 'delete':
+        elif v == 'delete' or 'remove':
             b.Minus()
 
         else:
-            print("Invalid input. Please type 'yes or no (show or delete)'")
+            print("Invalid input. Please type 'yes or no (show or delete(remove))'")
 
 if __name__ == '__main__':
     main()
